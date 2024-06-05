03e736e73c810c8c07083c701b1b357da66403a9
alexpoky2011 <alexpoky2011.dev@gmail.com>
2024-06-05 20:12:41 +0200

    njihugzyhuisdgfsaui69

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const cors = require('cors');
 const PORT = 40004;
 const logger = require('./src/utils/logger');
 
-const app = express(); // Initialize app here
-app.use(logger); // Now you can use app
+const app = express(); 
+app.use(logger); 
 app.use(cors());
+
 
 console.log('\x1b[33m● \x1b[0mStarting server...');
 
@@ -19,27 +20,28 @@ app.listen(
     () => console.log(`\x1b[32m● \x1b[0mOnline: \x1b[4mhttp://localhost:${PORT} \x1b[0m`)
 );
 
-app.get('/portfolio', (req, res) => {
-    const projects = [
-      {
-        id: 2,
-        image: 'http://localhost:5173/portfolio.png',
-        name: 'AlexPoky2011',
-        language: 'TypeScript',
-        description: 'Test Skibidy toilet kaicenat rizz',
-        date: '30.4.2024',
-        url: 'https://alexpoky2011.fun'
-      },
-      {
-        id: 1,
-        image: 'http://localhost:5173/portfolio.png',
-        name: 'AlexPoky2011',
-        language: 'TypeScript',
-        description: 'Test Skibidy toilet kaicenat rizz',
-        date: '30.4.2024',
-        url: 'https://alexpoky2011.fun'
-      },
 
-    ];
-    res.json(projects);
-  });
+app.get('/portfolio', (req, res) => {
+  const projects = [
+    {
+      id: 2,
+      image: 'http://localhost:5173/portfolio.png',
+      name: 'Nesnasim Negry',
+      language: 'TypeScript',
+      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+      date: '30.4.2024',
+      url: 'https://alexpoky2011.fun'
+    },
+    {
+      id: 1,
+      image: 'http://localhost:5173/portfolio.png',
+      name: 'AlexPoky2011',
+      language: 'TypeScript',
+      description: 'Moje vlastní webová stránka, kde je mé portfolio a nějaké informace o mně.',
+      date: '30.4.2024',
+      url: 'https://alexpoky2011.fun'
+    },
+
+  ];
+  res.json(projects);
+});
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,17 +1,25 @@
 const fs = require('fs');
 const logger = (req, res, next) => {
-  const ipAddress = req.ip;
-  const timestamp = `${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`;
-  const logMessage = `[INFO] ${ipAddress} - ${timestamp}\n`;
-  fs.appendFile('./src/logs.txt', logMessage, (err) => {
+  const ipAddress = req.headers['x-forwarded-for'] || req.ip;
+  const date = new Date();
+  const timestamp = date.toLocaleTimeString('cs-CZ', {
+    timeZone: 'Europe/Prague',
+    hour12: false,
+    hour: '2-digit',
+    minute: '2-digit'
+  });
+  const dateString = date.toLocaleDateString('cs-CZ', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric'
+  });
+  const logMessage = `[INFO] ${ipAddress} - ${dateString} ${timestamp}\n`;
+  fs.appendFile('./src/dir/logs.txt', logMessage, (err) => {
     if (err) {
       console.error(err);
     }
   });
   next();
 };
-
-
-
 
 module.exports = logger;
