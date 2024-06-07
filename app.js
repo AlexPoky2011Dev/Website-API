df0a75cfb813378b87e88b8b6fcf3789afa52fc6
alexpoky2011 <alexpoky2011.dev@gmail.com>
2024-06-07 15:25:58 +0200

    update

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const cors = require('cors');
 const PORT = 40004;
 const logger = require('./src/utils/logger');
 const app = express();
-const ACCESS_KEY = 'GhJ4EeH2L9';
+const ACCESS_KEY = ('GhJ4EeH2L9');
 
 const middlewares = () => {
   app.use(logger);
   app.use(cors());
 }
+
+app.use(cors({
+  origin: '*', 
+  credentials: true, 
+}));
 
 app.use((req, res, next) => {
   const apiKey = Object.values(req.query)[0];
@@ -29,17 +34,8 @@ app.listen(
 app.get('/portfolio', (req, res) => {
   const projects = [
     {
-      id: 2,
-      image: 'https://yt3.googleusercontent.com/DQKHWL4ZSGJdb_XOc3MjWQrLwDuUwdmqTp7BKahvdTYEazfp0PqRF79eaHbsCPhPyGecLjL9UsM=s900-c-k-c0x00ffffff-no-rj',
-      name: 'Kaicenat Rizz',
-      language: 'TypeScript',
-      description: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-      date: '30.4.2024',
-      url: 'https://alexpoky2011.fun'
-    },
-    {
       id: 1,
-      image: 'http://localhost:5173/portfolio.png',
+      image: 'https://alexpoky2011/portfolio.png',
       name: 'AlexPoky2011',
       language: 'TypeScript',
       description: 'Moje vlastní webová stránka, kde je mé portfolio a nějaké informace o mně.',
